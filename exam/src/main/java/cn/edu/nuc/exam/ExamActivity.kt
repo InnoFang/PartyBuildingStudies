b67a4e882383e1e794e0b18c3310d21a35c9656b
InnoFang <q984941707@qq.com>
2017-08-13 22:20:22 +0800

    upload base project

---
+++ exam/src/main/java/cn/edu/nuc/exam/ExamActivity.kt
@@ -0,0 +1,12 @@
+package cn.edu.nuc.exam
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ExamActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ex_activity)
+    }
+}
---
+++ popularScience/src/main/java/cn/edu/nuc/popularscience/PopularScienceActivity.kt
@@ -0,0 +1,12 @@
+package cn.edu.nuc.popularscience
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class PopularScienceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ps_activity)
+    }
+}
