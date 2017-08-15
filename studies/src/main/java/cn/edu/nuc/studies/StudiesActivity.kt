97c48b6e77f5b9c6fb3f0299498c116c7e3fe866
InnoFang <q984941707@qq.com>
2017-08-15 09:52:25 +0800

    [ADD] new module : studies

---
+++ studies/src/main/java/cn/edu/nuc/studies/StudiesActivity.kt
@@ -0,0 +1,12 @@
+package cn.edu.nuc.studies
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class StudiesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sd_activity_studies)
+    }
+}
